21f2123a9353b381df330eee485929da8627740e
nguyennc11 <nguyennc11@fsoft.com.vn>
2022-12-29 22:28:27 +0700

    feat: setup custom hooks

---
+++ src/hooks/useInitialState.ts
@@ -0,0 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../rootReducer";
+import { getAuthSuccessfully } from "../slices/authSlice";
+import {
+  getInitialDataThunk,
+  saveQuestionAnswerThunk,
+  saveQuestionThunk,
+} from "../thunk";
+
+export const useInitialState = () => {
+  const question = useSelector((state: RootState) => state.questions);
+  const authUser = useSelector((state: RootState) => state.auth);
+  const users = useSelector((state: RootState) => state.users);
+  const dispatch = useDispatch();
+
+  const handleLogin = (userId: string) => {
+    dispatch(getAuthSuccessfully(userId));
+  };
+
+  const handleInitialState = () => {
+    getInitialDataThunk()(dispatch);
+  };
+
+  const handleSaveQuestion = (question: any) => {
+    saveQuestionThunk(question)(dispatch);
+  };
+
+  const handleSaveQuestionAnswer = (
+    authState: any,
+    questionId: any,
+    option: any
+  ) => {
+    saveQuestionAnswerThunk(authState, questionId, option)(dispatch);
+  };
+
+  return {
+    users: users.data,
+    questions: question.data,
+    authUser: authUser.data,
+    handleLogin,
+    handleInitialState,
+    handleSaveQuestion,
+    handleSaveQuestionAnswer,
+  };
+};
